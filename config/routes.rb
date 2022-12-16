4d237dbd46d3dd224382183db87ab514d832a152
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-16 05:05:22 +0000

    [Add 1216]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:show, :edit]
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  devise_for :users
   root to:'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
 end
