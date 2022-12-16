b8785bf086e3154c4ad073f90a66fad8da71b60e
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-16 07:50:52 +0000

    [Add 1216]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to:'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    #単数形なら/:idが含まれなくなる
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
